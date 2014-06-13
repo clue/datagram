c8d841ff66b1546b1301772631b20d67cf90c35f
Christian Lück <christian@lueck.tv>
2014-06-13 12:33:05 +0200

    Move remote client with DNS resolution to separate example

--- example/remote-client.php
+++ example/remote-client.php
@@ -1,11 +1,32 @@
 <?php
 
+// This example demonstrates how to connect a datagram client to a given remote
+// address and send/receive some data.
+//
+// Resolving hostnames requires the "react/dns" component in the "require"
+// keys in your composer.json, so make sure you have it installed like this:
+//
+// "require": {
+//    "react/dns": "0.3.*"
+// }
+//
+// This example allows you to set the remote UDP server address (which defaults
+// to a non-functional value). Make sure to start up your server first
+// (for example by running the server.php example) and adjust the following
+// server address:
+
+$address = 'default.com:1234';
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $loop = React\EventLoop\Factory::create();
-$factory = new Datagram\Factory($loop);
 
-$factory->createClient('localhost:1234')->then(function (Datagram\Socket $client) use ($loop) {
+$dnsFactory = new React\Dns\Resolver\Factory();
+$resolver = $dnsFactory->createCached('8.8.8.8', $loop);
+
+$factory = new Datagram\Factory($loop, $resolver);
+
+$factory->createClient($address)->then(function (Datagram\Socket $client) use ($loop) {
     $client->send('first');
 
     $client->on('message', function($message, $serverAddress, $client) {
